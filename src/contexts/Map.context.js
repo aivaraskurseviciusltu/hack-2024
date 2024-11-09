77262b69f6aeddad0f4a574b23914cecb5ec779e
Ruta Bendoraityte <Ruta.Bendoraityte@ibm.com>
2024-11-10 01:09:50 +0200

    init 11

--- src/contexts/Map.context.js
+++ src/contexts/Map.context.js
@@ -14,15 +14,13 @@ const MapContextProvider = ({ children }) => {
     },
     {
       description: "Provide elevators with sufficient space and accessible buttons at a reachable height.",
-      image: `${process.env.PUBLIC_URL + './assets/food.jpeg'}`,
+      image: `${process.env.PUBLIC_URL + './assets/elevator.jpg'}`,
       latitude: 54.69489908237602,
       longitude: 25.28025837421105,
       iconType: "Wheelchair",
     },
     {
       description: "Provide Braille menus and directories for easy access to information.",
-      image:
-          `${process.env.PUBLIC_URL + './assets/room.webp'}`,
       latitude: 54.693490273002475,
       longitude: 25.281228157788654,
       iconType: "Blind",
@@ -30,7 +28,7 @@ const MapContextProvider = ({ children }) => {
     {
       description: "Approaching parking area includes a toll-bar be aware",
       image:
-          `${process.env.PUBLIC_URL + './assets/bomb.jpeg'}`,
+          `${process.env.PUBLIC_URL + './assets/tollbar.jpg'}`,
       latitude: 54.69303610474269,
       longitude: 25.282898588851634,
       iconType: "Alert",
@@ -38,7 +36,7 @@ const MapContextProvider = ({ children }) => {
     {
       description: "Provide tablets or digital devices for customers to place orders and read information without needing to speak or hear.",
       image:
-          `${process.env.PUBLIC_URL + './assets/aid.jpeg'}`,
+          `${process.env.PUBLIC_URL + './assets/ilunch.png'}`,
       latitude: 54.693700503812785,
       longitude: 25.275498460675088,
       iconType: "Deaf",
@@ -46,16 +44,15 @@ const MapContextProvider = ({ children }) => {
     {
       description: "The elevator near entrance is temporarily out of service",
       image:
-          `${process.env.PUBLIC_URL + './assets/enemy.jpeg'}`,
+          `${process.env.PUBLIC_URL + './assets/brokenElevator.webp'}`,
       latitude: 54.69970960219711,
       longitude: 25.2833959662436,
       iconType: "Alert",
     },
-
     {
       description: "Provides ramp for those who cannot use stairs.",
       image:
-          `${process.env.PUBLIC_URL + './assets/help.jpeg'}`,
+          `${process.env.PUBLIC_URL + './assets/ramp.jpg'}`,
       latitude: 54.69955928345105,
       longitude: 25.286470374417043,
       iconType: "Wheelchair",
